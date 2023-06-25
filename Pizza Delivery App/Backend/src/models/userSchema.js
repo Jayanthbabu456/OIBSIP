46940082a4bf8875f9c41e59d2e03c397948e36a
Jayanthbabu456 <babujayanth456@gmail.com>
2023-06-25 12:49:28 +0530

    password encryption and compare added

--- Pizza Delivery App/Backend/src/models/userSchema.js
+++ Pizza Delivery App/Backend/src/models/userSchema.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles.js";
-
+import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema(
   {
     fname: {
@@ -34,4 +34,20 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Encryption of password before svaing
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// compare password
+userSchema.methods = {
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
+};
+
 export default mongoose.model("User", userSchema);
